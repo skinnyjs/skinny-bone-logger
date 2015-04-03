6e0867438b908aaec270c201d1468f27a6a8e04f
Ivan Shumkov <ivan@shumkov.ru>
2015-04-03 20:34:40 +0300

    fix message dupplication

--- bone.js
+++ bone.js
@@ -10,22 +10,10 @@ module.exports = function attachLogger(skinny, options) {
     });
 
     skinny.on('warning', function(error) {
-        skinny.logger.warn(getMessage(error), error.stack);
+        skinny.logger.warn(error.stack);
     });
 
     skinny.on('error', function(error) {
-        skinny.logger.error(getMessage(error), error.stack);
+        skinny.logger.error(error.stack);
     });
 };
-
-function getMessage(error) {
-    var message;
-
-    if (error.name !== error.message) {
-        message = error.name + ': ' + error.message
-    } else {
-        message = error.message;
-    }
-
-    return message;
-}
