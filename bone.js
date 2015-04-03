346ea23f1fc077f1024cd25ff17cdbe1eeb0e9c5
Ivan Shumkov <ivan@shumkov.ru>
2015-04-03 17:39:20 +0300

    add error stack

--- bone.js
+++ bone.js
@@ -1,8 +1,8 @@
+"use strict"
+
 var winston = require('winston');
 
 module.exports = function attachLogger(skinny, options) {
-    "use strict";
-
     skinny.logger = new winston.Logger({
         transports: [
             new winston.transports.Console(options)
@@ -10,10 +10,22 @@ module.exports = function attachLogger(skinny, options) {
     });
 
     skinny.on('warning', function(error) {
-        skinny.logger.warn(error.name + ': ' + error.message, error);
+        skinny.logger.warn(getMessage(error), error.stack);
     });
 
     skinny.on('error', function(error) {
-        skinny.logger.error(error.name + ': ' + error.message, error);
+        skinny.logger.error(getMessage(error), error.stack);
     });
+};
+
+function getMessage(error) {
+    var message;
+
+    if (error.name !== error.message) {
+        message = error.name + ': ' + error.message
+    } else {
+        message = error.message;
+    }
+
+    return message;
 }
